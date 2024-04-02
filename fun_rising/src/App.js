1bd90479973241fb460fa752c78bb852328ff799
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-04-02 15:01:21 +0530

    new1

--- fun_rising/src/App.js
+++ fun_rising/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
+import '../src/Assets/css/Nav.css'
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home';
+import Nav from './Components/Nav';
+
+
 
 function App() {
 
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-  
-        
-        
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
 
-      </Routes>
+
+
+
+        </Routes>
       </BrowserRouter>
-    
+      <Nav />
     </div>
   );
 }
