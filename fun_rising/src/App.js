308709e093f6e1b1f340ab9a66f528b61308b262
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-04-02 13:48:59 +0530

    new

---
+++ fun_rising/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Components/Home';
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+  
+        
+        
+
+      </Routes>
+      </BrowserRouter>
+    
+    </div>
+  );
+}
+
+export default App;
